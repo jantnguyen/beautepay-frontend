d90f66560f7642eeb9ed45ec666a413bde998447
Jan Nguyen <giang.nss.c2@gmail.com>
2021-12-31 14:28:10 -0600

    add admin dashboard

--- src/App.vue
+++ src/App.vue
@@ -32,29 +32,70 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Home</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
+  <v-main>
+    <v-container>
+      <v-navigation-drawer permanent expand-on-hover>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Admin
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Empower your managing
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    </v-container>
+    <Home/>
+
+  </v-main>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+import Home from './components/Home';
+  export default {
+    name: 'App',
+    components: {
+    Home,
   },
-
-  data: () => ({
-    //
-  }),
-};
+    data () {
+      return {
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'Manage Team', icon: 'mdi-account-group-outline'},
+          {title: 'Manage Transaction', icon: 'mdi-cash-multiple'}
+        ],
+        right: null,
+      }
+    },
+  }
 </script>
